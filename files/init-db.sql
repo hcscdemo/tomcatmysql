3db1fb71df8abfa8e6acec2945fcd39f0249bd5c
chris <chrisyflai@gmail.com>
2017-03-28 14:58:43 +0800

    init

---
+++ files/init-db.sql
@@ -0,0 +1,9 @@
+create table employees (
+   id int,
+   first_name varchar(50),
+   last_name varchar(50),
+   primary key (id) );
+
+insert into employees values (1001, 'Chris', 'Lai');
+insert into employees values (1002, 'Loki', 'Ng');
+insert into employees values (1003, 'Frank', 'Hui');
